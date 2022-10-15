740fd3bf6f5a09db92ee9506de27439199661eb6
Rivaldi Aliando <rivdew@gmail.com>
2022-10-15 23:35:13 +0700

    Poliklinik

--- resources/views/pages/apotek/dashboard.blade.php
+++ resources/views/pages/apotek/dashboard.blade.php
@@ -7,20 +7,18 @@
 
 @section('custom_scripts')
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-<<<<<<< HEAD
-    <script></script>
-=======
     <script>
-
         const monthNames = ["January", "February", "March", "April", "May", "June",
             "July", "August", "September", "October", "November", "December"
         ];
-        
+
         function time() {
             let time = $('#time');
             let d = new Date();
 
-            let month = d.toLocaleString('default', { month: 'long' });
+            let month = d.toLocaleString('default', {
+                month: 'long'
+            });
             let day = d.getUTCDate();
             let year = d.getUTCFullYear();
 
@@ -31,22 +29,22 @@
             let m = d.getMinutes();
             let h = d.getHours();
             let show = ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2);
-            time.html(newdate +' | '+show) 
+            time.html(newdate + ' | ' + show)
         }
 
         setInterval(time, 1000);
 
         function detail(e) {
-            let id  = $(e).data('id')
-            let url = "{{route('medicine.list.medical', ":id")}}".replace(":id", id)
+            let id = $(e).data('id')
+            let url = "{{ route('medicine.list.medical', ':id') }}".replace(":id", id)
 
             ajaxRequest('GET', url).then((res) => {
                 let html = ''
                 $('#list-medicine').html(html);
-                if(res.result) {
-                    res.result.forEach((value) => { 
+                if (res.result) {
+                    res.result.forEach((value) => {
                         console.log(value)
-                         html = `<tr>
+                        html = `<tr>
                                 <td>${value.stock.name}</td>
                                 <td>${value.quantity} pcs</td>
                                 <td>${value.stock.quantity} pcs</td>
@@ -66,23 +64,22 @@
                     $('#list-medicine').html(html);
                 }
             }).catch((err) => {
-                
+
             });
 
             $('button[type=submit]').attr('data-id', id);
             $('#formModal').modal('toggle')
         }
 
-        function submitForm(el) 
-        {
-            let id      = $(el).data('id')
-            let input   = []
-            let url     = '{{route("medicine.list.approve", ":id")}}'.replace(":id", id)
-            document.querySelectorAll('input[type=checkbox]').forEach( (element, index) => {
+        function submitForm(el) {
+            let id = $(el).data('id')
+            let input = []
+            let url = '{{ route('medicine.list.approve', ':id') }}'.replace(":id", id)
+            document.querySelectorAll('input[type=checkbox]').forEach((element, index) => {
                 input[index] = {
-                    id      : $(element).data('id'),
+                    id: $(element).data('id'),
                     quantity: $(element).data('id'),
-                    status  : $(element).is(':checked'),
+                    status: $(element).is(':checked'),
                 }
             });
 
@@ -104,7 +101,6 @@
             $('#formModal').modal('toggle')
         }
     </script>
->>>>>>> 9d9754f6a720163ab52e4482eebda19b9c750ff3
 @endsection
 
 @section('content')
@@ -116,13 +112,6 @@
                     <div class="col-12">
                         <div class="card">
                             <div class="card-header">
-<<<<<<< HEAD
-                                <h5 class="title text-right" style="color: white">Rabu, 13 Oktober 2022 | 14:00 WIB</h5>
-                            </div>
-
-                            <div class="card-footer text-center">
-                                {{ date('Y') }} @ Konoha Hospitally
-=======
                                 <h5 class="title text-right" style="color: white" id="time">-: -:-:-</h5>
                             </div>
                             <div class="card-body">
@@ -146,27 +135,28 @@
                                         </thead>
                                         <tbody>
                                             @if (count($medical_record))
-                                            @foreach ($medical_record as $item)
+                                                @foreach ($medical_record as $item)
+                                                    <tr>
+                                                        <td>{{ $loop->iteration }}</td>
+                                                        <td>{{ $item->patient->name }} </td>
+                                                        <td>{{ $item->patient->nik }} </td>
+                                                        <td>{{ $item->user->name }} </td>
+                                                        <td>{{ $item->user->polikliniks[0]->name }} </td>
+                                                        <td>
+                                                            @foreach ($item->medicineLists as $medicine)
+                                                                <div>{{ $medicine->stock->name }} </div>
+                                                            @endforeach
+                                                        </td>
+                                                        <td>
+                                                            <a href="javascript:void(0)" onclick="javascript:detail(this)"
+                                                                data-id="{{ $item->id }}">Detail</a>
+                                                        </td>
+                                                    </tr>
+                                                @endforeach
+                                            @else
                                                 <tr>
-                                                    <td>{{$loop->iteration}}</td>
-                                                    <td>{{$item->patient->name}} </td>
-                                                    <td>{{$item->patient->nik}} </td>
-                                                    <td>{{$item->user->name}} </td>
-                                                    <td>{{$item->user->polikliniks[0]->name}} </td>
-                                                    <td> 
-                                                        @foreach ($item->medicineLists as $medicine)
-                                                            <div>{{$medicine->stock->name}} </div>
-                                                        @endforeach
-                                                    </td>
-                                                    <td>
-                                                        <a href="javascript:void(0)" onclick="javascript:detail(this)" data-id="{{$item->id}}">Detail</a>
-                                                    </td>
+                                                    <td colspan="7"> -- Belum Ada Data --</td>
                                                 </tr>
-                                            @endforeach
-                                            @else 
-                                            <tr>
-                                                <td colspan="7"> -- Belum Ada Data --</td>
-                                            </tr>
                                             @endif
                                         </tbody>
                                     </table>
@@ -174,8 +164,7 @@
 
                             </div>
                             <div class="card-footer text-center">
-                                {{date('Y')}} @ Konoha Hospitally
->>>>>>> 9d9754f6a720163ab52e4482eebda19b9c750ff3
+                                {{ date('Y') }} @ Konoha Hospitally
                             </div>
                         </div>
                     </div>
@@ -185,43 +174,40 @@
     </div>
     <!-- END MAIN CONTENT-->
 @endsection
-<<<<<<< HEAD
-=======
 
 @section('modal')
-<div class="modal fade" id="formModal" tabindex="-1" role="dialog" aria-labelledby="formModalLabel" aria-hidden="true"
-data-backdrop="static">
-   <div class="modal-dialog modal-lg" role="document">
-       <div class="modal-content">
-           <div class="modal-header">
-               <h5 class="modal-title" id="formModalLabel">Daftar Obat Yang Di Keluarkan</h5>
-               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                   <span aria-hidden="true">&times;</span>
-               </button>
-           </div>
-           <div class="modal-body">
-                <div class="card-body card-block">
-                    <table class="table table-bordered">
-                        <thead>
-                            <tr>
-                                <th>Item</th>
-                                <th>Stock Dibutuhkan</th>
-                                <th>Stock Tersisa</th>
-                                <th>Approval</th>
-                            </tr>
-                        </thead>
-                        <tbody id="list-medicine"></tbody>
-                    </table>
+    <div class="modal fade" id="formModal" tabindex="-1" role="dialog" aria-labelledby="formModalLabel" aria-hidden="true"
+        data-backdrop="static">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="formModalLabel">Daftar Obat Yang Di Keluarkan</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
                 </div>
-           </div>
-           <div class="modal-footer">
-               <button type="reset" class="btn btn-secondary" onclick="resetForm()">Cancel</button>
-               <button type="submit" class="btn btn-success" onclick="submitForm(this)">Confirm</button>
-           </div>
-       </div>
-   </div>
-</div>
+                <div class="modal-body">
+                    <div class="card-body card-block">
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>Item</th>
+                                    <th>Stock Dibutuhkan</th>
+                                    <th>Stock Tersisa</th>
+                                    <th>Approval</th>
+                                </tr>
+                            </thead>
+                            <tbody id="list-medicine"></tbody>
+                        </table>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="reset" class="btn btn-secondary" onclick="resetForm()">Cancel</button>
+                    <button type="submit" class="btn btn-success" onclick="submitForm(this)">Confirm</button>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
 {{-- $('input[type=checkbox]').is(':checked') --}}
->>>>>>> 9d9754f6a720163ab52e4482eebda19b9c750ff3
