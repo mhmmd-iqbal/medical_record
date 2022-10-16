7da5c1e6dec538177f8077911351ab1574b3fcf9
Rivaldi Aliando <rivdew@gmail.com>
2022-10-16 10:27:30 +0700

    Append Attribute Age

--- app/Http/Controllers/QueueController.php
+++ app/Http/Controllers/QueueController.php
@@ -27,7 +27,7 @@ class QueueController extends Controller
 
     public function indexApi()
     {
-        $queues = Queue::with('patient.medicalRecords.medicineLists', 'poliklinik')->get();
+        $queues = Queue::with('patient.medicalRecords.medicineLists.stock', 'poliklinik')->get();
         return $queues;
     }
 
